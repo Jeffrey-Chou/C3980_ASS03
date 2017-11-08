4cae811b61cb437111d7791556baa546239c03bf
Chia-Chu Chou <jeffreychou1991@gmail.com>
2017-11-07 18:41:02 -0800

    fsafsdfds

--- C3980_ASS03/gpsprint.c
+++ C3980_ASS03/gpsprint.c
@@ -111,8 +111,8 @@ void FillSatUsedList( bool* usedList, struct gps_data_t* gpsdata)
 
 void PrintSatelliteDetails(struct satellite_t* skyview)
 {
-    printf("PRN: %3d Elevation: %02d Azimuth: %03d SNR: %02f Used: %c\n",
-    skyview->PRN, skyview->elevation, skyview->azimuth, skyview->ss, ((skyview->used)? 'Y': 'N'));
+    printf("PRN: %3d Elevation: %02d Azimuth: %03d SNR: %02f Used: %c used is %d\n",
+    skyview->PRN, skyview->elevation, skyview->azimuth, skyview->ss, ((skyview->used)? 'Y': 'N'), skyview->used);
 
 }
 
